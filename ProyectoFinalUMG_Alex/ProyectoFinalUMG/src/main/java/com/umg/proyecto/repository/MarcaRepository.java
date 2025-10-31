a44b7f44dc2599309ec5fe5f77ee8ea8d77cf1f6
Quenedith <qaguirrea@miumg.edu.gt>
2025-10-30 21:34:17 -0600

    Delete ProyectoFinalUMG_Alex/ProyectoFinalUMG directory

--- ProyectoFinalUMG_Alex/ProyectoFinalUMG/src/main/java/com/umg/proyecto/repository/MarcaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.umg.proyecto.repository;
-import com.umg.proyecto.model.Marca;
-import java.util.*;
-public interface MarcaRepository {
-    List<Marca> findAll();
-    Optional<Marca> findById(int id);
-    Marca save(Marca m);
-    void delete(int id);
-}
